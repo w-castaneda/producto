3cc25a4aa4318019f2557263190b08901e6572b6
Wilfredo Castaneda <wilfredo.castaneda.guerrero@gmail.com>
2024-09-03 17:01:10 -0600

    Agregar DTO capa
    Agregar funcionalidad de ProductoDTO

---
+++ src/main/java/com/service/productos/dto/ProductoDTO.java
@@ -0,0 +1,33 @@
+package com.service.productos.dto;
+
+public class ProductoDTO {
+
+    private String id;
+    private String nombre;
+    private double precio;
+
+    // Getters y Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
